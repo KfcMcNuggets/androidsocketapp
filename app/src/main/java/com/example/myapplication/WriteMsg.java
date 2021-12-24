eff9ec73af8010ba845c375c633d6f4f6ede6919
imnixan <vcmvenom@gmail.com>
2021-12-24 18:39:45 +0300

    starting with crypter

--- app/src/main/java/com/example/myapplication/WriteMsg.java
+++ app/src/main/java/com/example/myapplication/WriteMsg.java
@@ -21,8 +21,7 @@ public class WriteMsg extends Thread {
 //        for (Integer i = 0; i<100  ; i++) {
             try {
                 ObjectOutputStream objectOutputStream = new ObjectOutputStream(clientSocket.getOutputStream());
-               // msg.msg = "ddos" + i + (new String(new char[100 - i]).replace("\0", "-"));
-                objectOutputStream.writeObject(msg);
+               objectOutputStream.writeObject(msg);
                 System.out.println("Plez send " + msg.msg + " to " + msg.receiver);
                 counter++;
 
